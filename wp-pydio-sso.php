cbbd1ee4667bd514858697766e1505e6f75116c4
Christian Foellmann <foellmann@foe-services.de>
2014-02-16 21:06:31 +0100

    integration of code by @pryley

--- wp-pydio-sso.php
+++ wp-pydio-sso.php
@@ -52,7 +52,7 @@ class WP_Pydio_SSO {
 
 	public $options;
 	public $glueCode;
-	public $glueCodeFound;
+	public $glueCode_found;
 
 	/**
 	 * Constructor. Hooks all interactions to initialize the class.
@@ -70,18 +70,22 @@ class WP_Pydio_SSO {
 
 		$this->options = get_option( 'pydio_settings' );
 		$this->glueCode = $this->options['install_path'] . '/plugins/auth.remote/glueCode.php';
-		$this->glueCodeFound = @is_file( $this->glueCode );
-		$this->autoCreate = $this->options['auto_create'];
+		$this->glueCode_found = @is_file( $this->glueCode );
 
+		// Authentication
 		add_action( 'wp_login',			array( $this, 'authenticate' ), 10, 2 );
 		add_action( 'wp_logout',		array( $this, 'logout' ), 1 );
 		add_action( 'user_register',	array( $this, 'createUser' ), 1, 1 );
 		add_action( 'set_user_role',	array( $this, 'updateUserRole' ), 1, 1 );
 		add_action( 'delete_user',		array( $this, 'deleteUser' ), 1, 1);
+
+		// Settings
 		add_action( 'admin_init',		array( $this, 'admin_init' ) );
+		add_action( 'admin_head',		array( $this, 'admin_head' ) );
 		add_action( 'admin_menu',		array( $this, 'admin_menu' ) );
 		add_action( 'plugins_loaded',	array( $this, 'load_plugin_textdomain' ) );
 
+		// Plugin Activation
 		register_activation_hook( __FILE__, array( 'WP_Pydio_SSO', 'activate_plugin' ) );
 
 	} // END __construct()
@@ -89,8 +93,8 @@ class WP_Pydio_SSO {
 	public function get_defaults() {
 
 		$defaults = array(
-			'install_path'	=> 'http://vvv.dev/pydio/core/src/', // @todo clear this!!
-			'secret_key'	=> 'sdafsdfasdfdd',
+			'install_path'	=> '',
+			'secret_key'	=> '',
 			'auto_create'	=> true,
 		);
 
@@ -100,93 +104,111 @@ class WP_Pydio_SSO {
 
 	} // END get_defaults()
 
-	public function authenticate( $username ) {
+	public function set_glue_globals( $authenticate, $user = null, $bool = null ) {
 
-		if( !$this->glueCodeFound ) {
+		if ( ! $this->glueCode_found ) {
 			return;
 		}
 
-		$userdata = get_user_by( 'login', $username );
-
 		global $AJXP_GLUE_GLOBALS;
+
 		$AJXP_GLUE_GLOBALS = array();
-		//$plugInAction, $login, $result, $secret, $autoCreate;
-		$AJXP_GLUE_GLOBALS['secret'] = $this->options['secret_key'];
-		$AJXP_GLUE_GLOBALS['autoCreate'] = $this->options['auto_create'];
-		$AJXP_GLUE_GLOBALS['plugInAction'] = 'login';
-		$AJXP_GLUE_GLOBALS['login'] = array(
-			'name' => $username,
-			'password' => $userdata->user_pass,
-			'roles' => $userdata->roles
-		);
+
+		switch ( $type ) {
+			case 'authenticate':
+				$AJXP_GLUE_GLOBALS['secret']			= $this->options['secret_key'];
+				$AJXP_GLUE_GLOBALS['autoCreate']		= $this->options['auto_create'];
+				$AJXP_GLUE_GLOBALS['plugInAction']		= 'login';
+				$AJXP_GLUE_GLOBALS['login']				= array(
+					'name'		=> $user,
+					'password'	=> $user->user_pass,
+					'roles'		=> $user->roles
+				);
+				break;
+			case 'logout':
+				$AJXP_GLUE_GLOBALS['secret'] 			= $this->options['secret_key'];
+				$AJXP_GLUE_GLOBALS['plugInAction']		= 'logout';
+				break;
+			case 'create_user':
+				$AJXP_GLUE_GLOBALS['user']				= array();
+				$AJXP_GLUE_GLOBALS['user']['name']		= $user->user_login;
+				$AJXP_GLUE_GLOBALS['user']['password']	= $user->user_pass;
+				$AJXP_GLUE_GLOBALS['user']['right']		= ( is_super_admin( $user->ID ) ? 'admin' : '' ); // @todo
+				$AJXP_GLUE_GLOBALS['user']['roles']		= $user->roles;
+				$AJXP_GLUE_GLOBALS['plugInAction']		= ( $bool ? 'addUser' : 'updateUser' );
+				break;
+			case 'delete_user':
+				$AJXP_GLUE_GLOBALS['secret']			= $this->options['secret_key'];
+				$AJXP_GLUE_GLOBALS['userName']			= $user->user_login;
+				$AJXP_GLUE_GLOBALS['plugInAction']		= "delUser";
+				break;
+		}
 
 		include( $this->glueCode );
+
+	} // END set_glue_globals()
+
+	public function authenticate( $username ) {
+
+		$this->set_glue_globals( 'authenticate', get_user_by( 'login', $username ) );
 
 	} // END authenticate()
 
-	 public function logout() {
+	public function logout() {
 
-		global $plugInAction;
-
-		if ( !$this->glueCodeFound ) {
-			return;
-		}
-
-		global $AJXP_GLUE_GLOBALS;
-
-		$AJXP_GLUE_GLOBALS					= array();
-		$AJXP_GLUE_GLOBALS['secret']		= $this->options['secret_key'];
-		$AJXP_GLUE_GLOBALS['plugInAction']	= 'logout';
-
-		include( $this->glueCode );
+		$this->set_glue_globals( 'logout' );
 
 	} // END logout()
 
-	public function updateUserRole($userId) {
+	public function update_user_role( $user_id ) {
 
-		$this->createUser( $userId, false );
+		$this->create_user( $user_id, false );
 
-	} // END updateUserRole()
+	} // END update_user_role()
 
-	public function createUser( $userId, $isNew = true ) {
+	public function create_user( $user_id, $is_new = true ) {
 
-		if ( !$this->glueCodeFound ) {
-			return;
+		$this->set_glue_globals( 'create_user', get_userdata( $user_id ), $is_new );
+
+	} // END create_user()
+
+	public function delete_user( $user_id ) {
+
+		$this->set_glue_globals( 'delete_user', get_userdata( $user_id ) );
+
+	} // END delete_user()
+
+	/**
+	 * Add a link to the admin bar to launch Pydio
+	 *
+	 * @since	2.1
+	 * @return	void
+	 */
+	public function admin_head() {
+
+		if ( ! empty( $this->options['install_path'] ) && current_user_can( apply_filters( 'wp_pydio_bridge_admin_bar_cap', 'activate_plugins' ) ) ) {
+
+			echo "<style type='text/css'>#wp-admin-bar-pydio > a:before{ content:'\\f322' }</style>";
+
+			add_action( 'wp_before_admin_bar_render', array( $this, 'admin_bar_switch' ), 99 );
 		}
 
-		$userData = get_userdata( $userId );
-		global $AJXP_GLUE_GLOBALS;
-		$AJXP_GLUE_GLOBALS = array();
-		//global $plugInAction, $result, $secret, $user;
-		$AJXP_GLUE_GLOBALS['secret']			= $this->options['secret_key'];
-		$AJXP_GLUE_GLOBALS['user']				= array();
-		$AJXP_GLUE_GLOBALS['user']['name']		= $userData->user_login;
-		$AJXP_GLUE_GLOBALS['user']['password']	= $userData->user_pass;
-		$AJXP_GLUE_GLOBALS['user']['right']		= ( is_super_admin( $userId ) ? 'admin' : '' ); // @todo
-		$AJXP_GLUE_GLOBALS['user']['roles']		= $userData->roles;
-		$AJXP_GLUE_GLOBALS['plugInAction']		= ( $isNew ? 'addUser' : 'updateUser' );
+	} // END admin_head()
 
-		include( $this->glueCode );
+	public function admin_bar_switch() {
 
-	} // END createUser()
+		global $wp_admin_bar;
+		$pydio_path = explode( $_SERVER['SERVER_NAME'], $this->options['install_path'] );
 
-	public function deleteUser( $userId ) {
+		$wp_admin_bar->add_node(
+			array(
+				'id' => 'pydio',
+				'title' => __( 'File Manager', 'wp-pydio-bridge' ),
+				'href' => 'http://' . $_SERVER['SERVER_NAME'] . $pydio_path[1],
+			)
+		);
 
-		if ( !$this->glueCodeFound ) {
-			return;
-		}
-
-		$userData = get_userdata( $userId );
-		global $AJXP_GLUE_GLOBALS;
-		$AJXP_GLUE_GLOBALS = array();
-		//global $plugInAction, $result, $secret, $userName;
-		$AJXP_GLUE_GLOBALS['secret']		= $this->options['secret_key'];
-		$AJXP_GLUE_GLOBALS['userName']		= $userData->user_login;
-		$AJXP_GLUE_GLOBALS['plugInAction']	= "delUser";
-
-		include( $this->glueCode );
-
-	} // END deleteUser()
+	} // END admin_bar_switch()
 
 	public function admin_menu() {
 
@@ -196,7 +218,7 @@ class WP_Pydio_SSO {
 			__( 'Pydio SSO', 'wp-pydio-sso' ),
 			apply_filters( 'wp_pydio_sso_settings_cap', 'activate_plugins' ), // @todo
 			'wp-pydio-sso',
-			array( &$this, 'options_page' )
+			array( $this, 'options_page' )
 		);
 
 	} // END admin_menu()
@@ -205,7 +227,7 @@ class WP_Pydio_SSO {
 
 		<div class="wrap">
 			<h2><?php _e( 'Pydio SSO Settings', 'wp-pydio-sso' ); ?></h2>
-			<form action="" method="post">
+			<form action="options.php" method="post">
 			<?php
 				settings_fields( 'pydio_settings_nonce' );
 				do_settings_sections( 'pydio_settings' );
@@ -222,20 +244,20 @@ class WP_Pydio_SSO {
 		register_setting(
 			'pydio_settings',
 			'pydio_settings',
-			array( &$this, 'plugin_options_validate' )
+			array( $this, 'plugin_options_validate' )
 		);
 
 		add_settings_section(
 			'pydio_settings_creds',
 			__( 'Credentials', 'wp-pydio-sso' ),
-			array( &$this, 'section_creds' ),
+			array( $this, 'section_creds' ),
 			'pydio_settings'
 		);
 
 		add_settings_field(
 			'pydio_text_string',
 			__( 'Pydio Path', 'wp-pydio-sso' ),
-			array( &$this, 'plugin_setting_string' ),
+			array( $this, 'plugin_setting_string' ),
 			'pydio_settings',
 			'pydio_settings_creds'
 		);
@@ -243,7 +265,7 @@ class WP_Pydio_SSO {
 		add_settings_field(
 			'pydio_secret_string',
 			__( 'Secret Key', 'wp-pydio-sso' ),
-			array( &$this, 'plugin_secret_string' ),
+			array( $this, 'plugin_secret_string' ),
 			'pydio_settings',
 			'pydio_settings_creds'
 		);
@@ -251,14 +273,14 @@ class WP_Pydio_SSO {
 		add_settings_section(
 			'pydio_settings_options',
 			__( 'Options', 'wp-pydio-sso' ),
-			array( &$this, 'section_options' ),
+			array( $this, 'section_options' ),
 			'pydio_settings'
 		);
 
 		add_settings_field(
 			'pydio_autocreate_string',
 			__( 'Auto Create', 'wp-pydio-sso' ),
-			array( &$this, 'plugin_autocreate_string' ),
+			array( $this, 'plugin_autocreate_string' ),
 			'pydio_settings',
 			'pydio_settings_options'
 		);
@@ -266,7 +288,7 @@ class WP_Pydio_SSO {
 		add_settings_section(
 			'pydio_settings_repo',
 			__( 'Creating Pydio Repositories', 'wp-pydio-sso' ),
-			array( &$this, 'section_repo' ),
+			array( $this, 'section_repo' ),
 			'pydio_settings'
 		);
 
@@ -275,34 +297,27 @@ class WP_Pydio_SSO {
 	public function guess_pydio_path() {
 
 		//get WP abs path
-		$wp_abspath = ABSPATH;
-		$url = dirname( $wp_abspath );
+		$path = dirname( ABSPATH ) . '/pydio';
 
-		return $url . '/pydio';
+		return $path;
 
 	} // END guess_pydio_path()
 
-	public function section_creds() { ?>
+	public function section_creds() {
 
-		<p>
-			<?php _e( 'Set the data to connect to your Pydio installation', 'wp-pydio-sso' ); ?>
-		</p>
+		echo "<p>" . __( 'Set the data to connect to your Pydio installation', 'wp-pydio-sso' ) . "</p>";
 
-	<?php
 	} // END install_section_text()
 
-	public function section_options() { ?>
+	public function section_options() {
 
-		<p>
-			<?php _e( 'Define how WordPress and Pydio should interact', 'wp-pydio-sso' ); ?>
-		</p>
+		echo "<p>" . __( 'Define how WordPress and Pydio should interact', 'wp-pydio-sso' ) . "</p>";
 
-	<?php
 	} // END options_section_text()
 
 	public function section_repo() {
 
-		$installPath = str_replace( "\\", "/", dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) );
+		$uploads = wp_upload_dir();
 		?>
 		<p>
 			<?php _e( "Now that your Wordpress users can access Pydio, you have to create repositories in Pydio and let them access it. This is not automated at the moment, so you have to log in as 'admin' and create them manually from within Pydio. Set the repositories default rights at least at 'r' (read), so that the users can indeed access the repositories.", 'wp-pydio-sso' ); ?>
@@ -310,9 +325,9 @@ class WP_Pydio_SSO {
 		<p>
 			<?php _e( 'Repository creation will ask you to enter the path to your repository. Here are some wordpress-related paths you may want to explore using Pydio :', 'wp-pydio-sso' ); ?>
 			<ul>
-				<li><code>.<?php echo $installPath; ?>/wp-content/themes</code> : <?php _e( 'The wordpress themes contents', 'wp-pydio-sso' ); ?><li>
-				<li><code>.<?php echo $installPath; ?>/wp-content/plugins</code> : <?php _e( 'The wordpress plugins', 'wp-pydio-sso' ); ?><li>
-				<li><code>.<?php echo $installPath . '/' . get_option( 'upload_path' ); ?></code> : <?php _e( 'The media library', 'wp-pydio-sso' ); ?><li>
+				<li><code>.<?php echo get_theme_root(); ?></code> : <?php _e( 'The wordpress themes contents', 'wp-pydio-sso' ); ?><li>
+				<li><code>.<?php echo WP_PLUGIN_DIR; ?></code> : <?php _e( 'The wordpress plugins', 'wp-pydio-sso' ); ?><li>
+				<li><code>.<?php echo $uploads['basedir']; ?></code> : <?php _e( 'The media library', 'wp-pydio-sso' ); ?><li>
 			</ul>
 			<?php _e( 'Of course, repositories are not limited to these values, you can browse whatever part of you server', 'wp-pydio-sso' ); ?>
 		</p>
@@ -321,14 +336,14 @@ class WP_Pydio_SSO {
 
 	public function plugin_setting_string() {
 
-		echo '<input id="plugin_text_string" name="wp_ajxp_options[ajxp_install_path]" size="70" type="text" value="' . $this->options['install_path'] . '" />';
-		echo '<p class="description">' . sprintf( __( 'Installation path. Enter here the full path to your installation on the server, i.e. the root folder containing ajaxplorer index.php file. Do not include slash at the end. May look like %s', 'wp-pydio-sso' ), '<code>' . $this->guess_pydio_path() . '</code>') . '</p>';
+		echo "<input id='plugin_text_string' name='pydio_settings[install_path]' size='70' type='text' value='{$this->options['install_path']}' />";
+		echo '<p class="description">' . sprintf( __( 'Installation path. Enter here the full path to your installation on the server, i.e. the root folder containing ajaxplorer index.php file. Do not include slash at the end. May look like %s', 'wp-pydio-sso' ), '<code>' . $this->guess_pydio_path() . '</code>' ) . '</p>';
 
 	} // END plugin_setting_string()
 
 	public function plugin_secret_string() {
 
-		echo '<input id="plugin_secret_string" name="wp_ajxp_options[ajxp_secret_key]" size="70" type="text" value="' . $this->options['secret_key'] . '" />';
+		echo "<input id='plugin_secret_string' name='pydio_settings[secret_key]' size='70' type='text' value='{$this->options['secret_key']}' />";
 		echo '<p class="description">' . __( "must be the same as the AUTH_DRIVER 'SECRET' option in your Pydio configuration", 'wp-pydio-sso' ) . '</p>';
 
 	} // END plugin_secret_string()
@@ -336,17 +351,15 @@ class WP_Pydio_SSO {
 	public function plugin_autocreate_string() {
 
 		$value = $this->options['auto_create'];
-		$true = ( true == $value );
-		$false = ( true != $value );
 		?>
 		<fieldset>
 			<label title="<?php _e( 'Yes', 'wp-pydio-sso' ); ?>">
-				<input type="radio" id="pydio_auto_create_true" value="true" name="pydio_settings[auto_create]" <?php checked( $true ); ?>>
+				<input type="radio" id="pydio_auto_create_true" value="1" name="pydio_settings[auto_create]" <?php checked( $value, true, false ); ?>/>
 				<span><?php _e( 'Yes', 'wp-pydio-sso' ); ?></span>
 			</label>
 			<br>
 			<label title="<?php _e( 'No', 'wp-pydio-sso' ); ?>">
-				<input type="radio" id="pydio_auto_create_false" value="false" name="pydio_settings[auto_create]" <?php checked( $false ); ?>>
+				<input type="radio" id="pydio_auto_create_false" value="0" name="pydio_settings[auto_create]" <?php checked( $value, true, false ); ?>/>
 				<span><?php _e( 'No', 'wp-pydio-sso' ); ?></span>
 			</label>
 			<p class="description"><?php _e( 'Create Pydio users when they login', 'wp-pydio-sso' ); ?></p>
@@ -357,7 +370,7 @@ class WP_Pydio_SSO {
 
 	public function plugin_options_validate( $input ) {
 
-		$newinput = array();
+		$newinput					= array();
 		$newinput['install_path']	= trim( $input['install_path'] );
 		$newinput['secret_key']		= trim( $input['secret_key'] );
 		$install					= $newinput['install_path'];
@@ -371,7 +384,7 @@ class WP_Pydio_SSO {
 			$newinput['install_path'] = "";
 		}
 
-		$newinput['auto_create'] = $input['auto_create'];
+		$newinput['auto_create'] = in_array( $input['auto_create'], array( '1', 1, 'true', true ) ) ? true : false;
 
 		return $newinput;
 
